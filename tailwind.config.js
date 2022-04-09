8960fa50fddf98bec3167bb4fe87ab4801d06c06
unknown <ibrahimacamara@gmx.fr>
2022-04-09 15:54:10 +0000

    Tailwindcss installed ok

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './resources/views/**/*.edge',
+    './resources/css/**/*.css',
+    './resources/js/**/*.js',
+    './resources/js/**/*.ts',
+
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
